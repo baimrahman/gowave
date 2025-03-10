53d389c9142203b07bdcaabfc1882bf2410a4be0
baimrahman <baimrahman86@gmail.com>
2025-03-10 12:39:45 +0700

    Update Location and Pop Up

--- src/data/paddlingLocations.ts
+++ src/data/paddlingLocations.ts
@@ -1,81 +1,156 @@
-import { PaddlingLocation, PaddlingService } from '../types';
+import { PaddlingLocation, PaddlingService } from "../types";
 
 export const paddlingLocations: PaddlingLocation[] = [
-  { 
-    position: [-6.115710, 106.858338], 
-    place: "Pantai Ancol", 
-    name: "Maliko Stand Up Paddle", 
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9haOEOe4MT-ym9kCo9r7vdb9OYHZznflCqw&s", 
-    priceStart: 350000, 
-    service: [PaddlingService.StandUpPaddle], 
-    whatsapp: "https://wa.me/6281234567890", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-6.115710,106.858338", 
-    images: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"] 
+  {
+    position: [-6.11571, 106.858338],
+    place: "Pantai Ancol",
+    name: "Maliko Stand Up Paddle",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9haOEOe4MT-ym9kCo9r7vdb9OYHZznflCqw&s",
+    priceStart: 350000,
+    service: [PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/6281399492183",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-6.115710,106.858338",
+    images: [],
   },
-  { 
-    position: [-6.392044, 106.752839], 
-    place: "Danau 7 Muara", 
-    name: "Ulawana SUP", 
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrEDFf0p1MURKyVy8Etyxk5pem3LRXsodaWA&s", 
-    priceStart: 300000, 
-    service: [PaddlingService.StandUpPaddle], 
-    whatsapp: "https://wa.me/6281234567890", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-6.392044,106.752839", 
-    images: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"] 
+  {
+    position: [-6.392044, 106.752839],
+    place: "Danau 7 Muara",
+    name: "Ulawana SUP",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrEDFf0p1MURKyVy8Etyxk5pem3LRXsodaWA&s",
+    priceStart: 300000,
+    service: [PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/6281314996382",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-6.392044,106.752839",
+    images: [],
   },
-  { 
-    position: [-6.113101, 106.863555], 
-    place: "Gelanggang Olah Raga Air Bahtera", 
-    name: "Klub Dayung Bahtera", 
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5KqQwMrQ9YDLQg2Y7bx7zgSSf_sjTQzVbWg&s", 
-    priceStart: 300000, 
-    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak], 
-    whatsapp: "https://wa.me/6281234567890", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-6.113101,106.863555", 
-    images: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"] 
+  {
+    position: [-6.113101, 106.863555],
+    place: "Gelanggang Olah Raga Air Bahtera",
+    name: "Klub Dayung Bahtera",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5KqQwMrQ9YDLQg2Y7bx7zgSSf_sjTQzVbWg&s",
+    priceStart: 300000,
+    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak],
+    whatsapp: "",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-6.113101,106.863555",
+    images: [],
   },
-  { 
-    position: [-6.883939, 107.451596], 
-    place: "Waduk Saguling", 
-    name: "Cathy Water Sports", 
-    logo: "https://picsum.photos/200/300", 
-    priceStart: 300000, 
-    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak], 
-    whatsapp: "https://wa.me/6281234567890", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-6.883939,107.451596", 
-    images: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"] 
+  {
+    position: [-6.883939, 107.451596],
+    place: "Waduk Saguling",
+    name: "Cathy Water Sports",
+    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/cathy-water/452534776_819799870255929_4979072964332299425_n.jpg",
+    priceStart: 300000,
+    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak],
+    whatsapp: "https://wa.me/6289639770909",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-6.883939,107.451596",
+    images: [],
   },
-  { 
-    position: [-7.951609, 110.424639], 
-    place: "Sungai Oya", 
-    name: "Ulawana SUP", 
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrEDFf0p1MURKyVy8Etyxk5pem3LRXsodaWA&s", 
-    priceStart: 200000, 
-    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak], 
-    whatsapp: "https://wa.me/6281234567890", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-7.951609,110.424639", 
-    images: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"] 
+  {
+    position: [-7.951609, 110.424639],
+    place: "Sungai Oya",
+    name: "Ulawana SUP",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrEDFf0p1MURKyVy8Etyxk5pem3LRXsodaWA&s",
+    priceStart: 200000,
+    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak],
+    whatsapp: "https://wa.me/6282144419125",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-7.951609,110.424639",
+    images: [],
   },
-  { 
-    position: [-7.821256, 110.115110], 
-    place: "Waduk Sermo", 
-    name: "Ulawana SUP", 
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrEDFf0p1MURKyVy8Etyxk5pem3LRXsodaWA&s", 
-    priceStart: 200000, 
-    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak], 
-    whatsapp: "https://wa.me/6281234567890", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-7.821256,110.115110", 
-    images: ["https://picsum.photos/200/300", "https://picsum.photos/200/300", "https://picsum.photos/200/300"] 
+  {
+    position: [-7.821256, 110.11511],
+    place: "Waduk Sermo",
+    name: "Ulawana SUP",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrEDFf0p1MURKyVy8Etyxk5pem3LRXsodaWA&s",
+    priceStart: 200000,
+    service: [PaddlingService.StandUpPaddle, PaddlingService.Kayak],
+    whatsapp: "https://wa.me/6282144419125",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-7.821256,110.115110",
+    images: [],
   },
-  { 
-    position: [-6.337011, 107.321109], 
-    place: "Danau Bintang Alam", 
-    name: "Veloce SUP", 
-    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/veloce-logo.png", 
-    priceStart: 300000, 
-    service: [PaddlingService.StandUpPaddle], 
-    whatsapp: "https://wa.me/6285183064291", 
-    direction: "https://www.google.com/maps/dir/?api=1&destination=-6.337011,107.321109", 
-    images: ["https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/photo_2025-03-05_23-39-12%20-%20Veloce%20Management.jpg", "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/photo_2025-03-05_23-39-16%20-%20Veloce%20Management.jpg", "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/photo_2025-03-05_23-39-20%20-%20Veloce%20Management.jpg"] 
-  }
-]; 
+  {
+    position: [-6.337011, 107.321109],
+    place: "Danau Bintang Alam",
+    name: "Veloce SUP",
+    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/veloce-logo.png",
+    priceStart: 300000,
+    service: [PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/6285183064291",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-6.337011,107.321109",
+    images: [
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/photo_2025-03-05_23-39-12%20-%20Veloce%20Management.jpg",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/photo_2025-03-05_23-39-16%20-%20Veloce%20Management.jpg",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/veloce-sup/photo_2025-03-05_23-39-20%20-%20Veloce%20Management.jpg",
+    ],
+  },
+  {
+    position: [-8.007925, 110.285739],
+    place: "Muara Baros",
+    name: "Ombak Selatan",
+    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/ombak-selatan/Logo%20-%20Hendra%20Nurdiyansyah.png",
+    priceStart: 200000,
+    service: [PaddlingService.Kayak, PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/628121856900",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-8.007925,110.285739",
+    images: [
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/ombak-selatan/IMG_1547.jpg",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/ombak-selatan/IMG_1548.jpg",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/ombak-selatan/IMG_1549.jpg",
+    ],
+  },
+  {
+    position: [-7.1577597, 112.8024761],
+    place: "Pantai Goa Petapa",
+    name: "Neira SUP",
+    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/neira-sup/neira.png",
+    priceStart: 150000,
+    service: [PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/6281332970382",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-7.1577597,112.8024761",
+    images: [
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/neira-sup/DSC06339-01%20-%20chris%20jose%20(1).jpeg",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/neira-sup/20240728_111415%20-%20chris%20jose%20(1).jpg",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/neira-sup/20240804_093948%20-%20chris%20jose%20(1).jpg",
+    ],
+  },
+  {
+    position: [-8.734181, 115.163286],
+    place: "Pantai Jerman",
+    name: "Bali SUP School",
+    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/balisupschool/balisupschool.jpg",
+    priceStart: 0,
+    service: [PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/628883676460",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-8.734181,115.163286",
+    images: [
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/balisupschool/Screenshot%202025-03-10%20at%2012.16.13.png",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/balisupschool/Screenshot%202025-03-10%20at%2012.15.46.png",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/balisupschool/Screenshot%202025-03-10%20at%2012.15.21.png",
+    ],
+  },
+  {
+    position: [-8.755091, 115.166074],
+    place: "Pantai Kelan",
+    name: "Priority SUP",
+    logo: "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/priority-sup/priority.jpg",
+    priceStart: 0,
+    service: [PaddlingService.StandUpPaddle],
+    whatsapp: "https://wa.me/6287862487923",
+    direction:
+      "https://www.google.com/maps/dir/?api=1&destination=-8.755091,115.166074",
+    images: [
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/priority-sup/Screenshot%202025-03-10%20at%2012.25.00.png",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/priority-sup/Screenshot%202025-03-10%20at%2012.25.38.png",
+      "https://andum.sgp1.cdn.digitaloceanspaces.com/gowave/priority-sup/Screenshot%202025-03-10%20at%2012.26.14.png",
+    ],
+  },
+];
